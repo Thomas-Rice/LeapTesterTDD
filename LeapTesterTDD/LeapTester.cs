1e608cf10ed3afd5b2ea2ed634c8cb38923f631c
Thomas-Rice <tikkleanim@gmail.com>
2017-08-29 17:15:22 +0100

    added first test to see if four is divisible by four

---
+++ LeapTesterTDD/LeapTester.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace LeapTesterTDD
+{
+    [TestFixture]
+    public class LeapTesterShould
+    {
+        [Test]
+        public void FourIsDivisibleByFour()
+        {
+            bool result = LeapYearIdentifier.CheckIfDivisibleByFour(4);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
